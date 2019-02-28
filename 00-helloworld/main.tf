74f7842f6eec9aba1a014868c8ce88cb7b4f1c48
yuki <yuki@yuki.local>
2019-02-28 22:30:06 +0000

    Add ec2 and eip

--- 00-helloworld/main.tf
+++ 00-helloworld/main.tf
@@ -77,27 +77,25 @@ resource "aws_security_group" "ssh" {
 }
 
 resource "aws_instance" "ec2-test" {
-  ami = "ami-07413a099547ecc89"
+  ami = "ami-04328208f4f0cf1fe"
   instance_type = "t2.micro"
-  key_name = "foo-key"
+  disable_api_termination = true
+  subnet_id = "${aws_subnet.public-a.id}"
   vpc_security_group_ids = [
-    "${aws_security_group.ssh.id}"
+    "${aws_security_group.ssh.id}",
   ]
 
-  subnet_id = "${aws_subnet.public-a.id}"
-  associate_public_ip_address = "true"
   root_block_device = {
     volume_type = "gp2"
     volume_size = "20"
   }
 
-  ebs_block_device = {
-    device_name = "/dev/sdf"
-    volume_type = "gp2"
-    volume_size = "100"
-  }
-
   tags {
     Name = "ec2-test"
   }
+}
+
+resource "aws_eip" "test-eip" {
+  instance = "${aws_instance.ec2-test.id}"
+  vpc      = true
 }
